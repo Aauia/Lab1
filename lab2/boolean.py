2b938ed19d46281d3e44a5848016cd90c3a7430f
Aauia <156919123+Aauia@users.noreply.github.com>
2024-01-25 15:37:55 +0600

    Add files via upload

---
+++ lab2/boolean.py
@@ -0,0 +1,20 @@
+#ex 1
+print(10>9)
+#Result: True
+
+#ex 2
+print(10==9)
+#Result: False 
+
+#ex 3
+print(10<9)
+#Result: False
+
+#ex 4
+print(bool("abc"))
+#Result: True
+
+#ex 5
+print(bool(0))
+#Result: False
+
